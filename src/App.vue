ec4b78a909103d10e038ed1deac47dffaca40957
Steve Doggett <steve@orbitalwitness.com>
2020-02-18 08:37:06 +0000

    Understanding Javascript animations

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,44 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
                 <hr />
-                <select @change="onAnimationChange" class="form-control">
-                    <option value="fade">Fade</option>
-                    <option value="slide">Slide</option>
-                </select>
+                <!--                <select @change="onAnimationChange" class="form-control">
+                                    <option value="fade">Fade</option>
+                                    <option value="slide">Slide</option>
+                                </select>
+                                <br /><br />
+                                <button @click="show = !show" class="btn btn-primary">Show alert</button>
+                                <br /><br />
+                                <transition :name="alertAnimation" :type="transitionType">
+                                    <div class="alert alert-info" v-if="show">This is some info</div>
+                                </transition>
+                                <transition name="slide" type="animation">
+                                    <div class="alert alert-info" v-if="show">This is some info</div>
+                                </transition>
+                                <transition
+                                        enter-active-class="animated bounce"
+                                        leave-active-class="animated shake"
+                                >
+                                    <div class="alert alert-info" v-if="show">This is some info</div>
+                                </transition>
+                                <transition :name="alertAnimation" mode="out-in">
+                                    <div class="alert alert-info" v-if="show">This is some info</div>>
+                                    <div class="alert warning" v-if="!show">This is some warning</div>
+                                </transition>-->
+                <button @click="load = !load" class="btn btn-primary">Load / Remove element</button>
                 <br /><br />
-                <button @click="show = !show" class="btn btn-primary">Show alert</button>
-                <br /><br />
-<!--                <transition :name="alertAnimation" :type="transitionType">
-                    <div class="alert alert-info" v-if="show">This is some info</div>
-                </transition>
-                <transition name="slide" type="animation">
-                    <div class="alert alert-info" v-if="show">This is some info</div>
-                </transition>
                 <transition
-                        enter-active-class="animated bounce"
-                        leave-active-class="animated shake"
+                        :css="false"
+                        @after-enter="afterEnter"
+                        @after-leave="afterLeave"
+                        @before-enter="beforeEnter"
+
+                        @before-leave="beforeLeave"
+                        @enter="enter"
+                        @enter-cancelled="enterCancelled"
+                        @leave="leave"
+                        @leave-cancelled="leaveCancelled"
                 >
-                    <div class="alert alert-info" v-if="show">This is some info</div>
-                </transition>-->
-                <transition :name="alertAnimation" mode="out-in">
-                    <div class="alert alert-info" v-if="show">This is some info</div>>
-                    <div class="alert warning" v-if="!show">This is some warning</div>
+                    <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load"></div>
                 </transition>
             </div>
         </div>
@@ -37,7 +53,36 @@
         data() {
             return {
                 show: false,
+                load: true,
                 alertAnimation: 'fade'
+            }
+        },
+        methods: {
+            beforeEnter(el) {
+                console.log('beforeEnter', el);
+            },
+            enter(el, done) {
+                console.log('enter', el);
+                done();
+            },
+            afterEnter(el) {
+                console.log('afterEnter', el);
+            },
+            enterCancelled(el) {
+                console.log('enterCancelled', el);
+            },
+            beforeLeave(el) {
+                console.log('beforeLeave', el);
+            },
+            leave(el, done) {
+                console.log('leave', el);
+                done();
+            },
+            afterLeave(el) {
+                console.log('afterLeave', el);
+            },
+            leaveCancelled(el) {
+                console.log('leaveCancelled', el);
             }
         }
     }
