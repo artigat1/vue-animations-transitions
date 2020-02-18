e9358ce7e51a66c04607d5e6228be265b057226c
Steve Doggett <steve@orbitalwitness.com>
2020-02-18 08:44:26 +0000

    Creating an animation in JS
    https://www.udemy.com/course/vuejs-2-the-complete-guide/learn/lecture/5975820#overview

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@
                         @leave="leave"
                         @leave-cancelled="leaveCancelled"
                 >
-                    <div style="width: 100px; height: 100px; background-color: lightgreen" v-if="load"></div>
+                    <div style="width: 300px; height: 100px; background-color: lightgreen" v-if="load"></div>
                 </transition>
             </div>
         </div>
@@ -54,35 +54,56 @@
             return {
                 show: false,
                 load: true,
-                alertAnimation: 'fade'
+                alertAnimation: 'fade',
+                elementWidth: 100
             }
         },
         methods: {
             beforeEnter(el) {
-                console.log('beforeEnter', el);
+                console.log('beforeEnter');
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth + 'px';
             },
             enter(el, done) {
-                console.log('enter', el);
-                done();
+                console.log('enter');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth + (round * 10)) + 'px';
+                    round++;
+                    if (round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
             },
             afterEnter(el) {
-                console.log('afterEnter', el);
+                console.log('afterEnter');
             },
             enterCancelled(el) {
-                console.log('enterCancelled', el);
+                console.log('enterCancelled');
             },
             beforeLeave(el) {
-                console.log('beforeLeave', el);
+                console.log('beforeLeave');
+                this.elementWidth = 300;
+                el.style.width = this.elementWidth + 'px'
             },
             leave(el, done) {
-                console.log('leave', el);
-                done();
+                console.log('leave');
+                let round = 1;
+                const interval = setInterval(() => {
+                    el.style.width = (this.elementWidth - (round * 10)) + 'px';
+                    round++;
+                    if (round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20);
             },
             afterLeave(el) {
-                console.log('afterLeave', el);
+                console.log('afterLeave');
             },
             leaveCancelled(el) {
-                console.log('leaveCancelled', el);
+                console.log('leaveCancelled');
             }
         }
     }
